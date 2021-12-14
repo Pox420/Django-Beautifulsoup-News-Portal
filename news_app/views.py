e0fce9d44e9b6aa860967437c4771f7ecfe81e44
pox420 <pox420@gmail.com>
2021-12-14 21:15:37 +0545

    done with exception handling if bs4 got an error while scraping data

--- news_app/views.py
+++ news_app/views.py
@@ -7,23 +7,30 @@ from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def index(request):
-    for i in soup.page_details:
-        title = i['title']
-        image_link = i['image_link']
-        sub_title = i['sub_title']
-        main_content = i['main_content']
+    try:
+        for i in soup.page_details:
+            title = i['title']
+            image_link = i['image_link']
+            sub_title = i['sub_title']
+            main_content = i['main_content']
 
-        if NewsPortal.objects.filter(title=title).exists():
-            pass
-        else:
-            NewsPortal.objects.create(title=title, image_link=image_link, sub_title=sub_title, main_content=main_content)
+            if NewsPortal.objects.filter(title=title).exists():
+                pass
+            else:
+                NewsPortal.objects.create(title=title, image_link=image_link, sub_title=sub_title, main_content=main_content)
 
-    news = NewsPortal.objects.all().order_by('-id')
+        news = NewsPortal.objects.all().order_by('-id')
 
-    context = {
-        'news':news,
-    }
-    return render(request, 'news_app/index.html',context=context)
+        context = {
+            'news':news,
+        }
+        return render(request, 'news_app/index.html',context=context)
+    except Exception as e:
+        news = NewsPortal.objects.all().order_by('-id')
+        context = {
+            'news':news,
+        }
+        return render(request, 'news_app/index.html',context=context)
 
 @login_required(login_url='/accounts/login/')
 def detail_page(request, id):
